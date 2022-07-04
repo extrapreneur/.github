c559131f462ed4de64615036b5515b2269671356
psilore <psiloc@gmail.com>
2022-07-04 07:49:38 +0200

    init

---
+++ index.js
@@ -0,0 +1,128 @@
+//adding libraries
+
+//const puppeteer = require('puppeteer');
+import puppeteer from "puppeteer";
+
+//const RssParser = require("rss-parser");
+import Parser from "rss-parser";
+let parser = new Parser();
+
+//const vfile = require("to-vfile");
+import { toVFile, readSync } from "to-vfile";
+
+//const remark = require("remark");
+import { remark } from "remark";
+
+//const zone = require("mdast-zone");
+import { zone } from "mdast-zone";
+
+//const { promisify } = require("util");
+import { promisify } from "util";
+
+//const { writeFile } = require("fs");
+import { writeFile } from "fs";
+
+//const { join } = require("path");
+import { join } from "path";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { constants } from "buffer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const readmePath = join(__dirname, "profile/README.md");
+
+// Rip about us first paragraph and blog links
+const getAboutUsString = await puppeteer.launch().then(async (browser) => {
+  //browser new page
+  const p = await browser.newPage();
+  //set viewpoint of browser page
+  await p.setViewport({ width: 1000, height: 500 });
+  await p.goto("https://www.extrapreneur.se/en/about-us");
+
+  const result = await p.evaluate(
+    () =>
+      document.querySelector("#block-a69218aa3038e836d047 > div > p").innerHTML
+  );
+
+  /*   await p.goto("https://www.extrapreneur.se/blog");
+
+  const blogPostLinks = await p.evaluate(() =>
+    Array.from(document.getElementsByTagName("h1"), (e) => e.innerHTML)
+  ); */
+
+  await browser.close();
+
+  return result;
+});
+
+const file = await remark()
+  .use(refreshAboutUsParagraph)
+  .process(toVFile.readSync(readmePath));
+await promisify(writeFile)(readmePath, String(file));
+
+function refreshAboutUsParagraph() {
+  let string = getAboutUsString;
+
+  return (tree) => {
+    zone(tree, "about", (start, nodes, end) => [
+      start,
+      {
+        type: "paragraph",
+        children: [{ type: "text", value: string }],
+      },
+      end,
+    ]);
+  };
+}
+
+(async () => {
+  const feed = await parser.parseURL(
+    "https://www.extrapreneur.se/blog?format=rss"
+  );
+  const file = await remark()
+    .use(refreshBlogPosts(feed.items.slice(0, 5)))
+    .process(toVFile.readSync(readmePath));
+  await promisify(writeFile)(readmePath, String(file));
+})();
+
+function refreshBlogPosts(feedItems) {
+  return () => (tree) => {
+    zone(tree, "blog", (start, nodes, end) => {
+      return [
+        start,
+        {
+          type: "list",
+          ordered: false,
+          children: feedItems.map(
+            ({ title, link, contentSnippet, pubDate }) => {
+              return {
+                type: "listItem",
+                children: [
+                  {
+                    type: "paragraph",
+                    children: [
+                      {
+                        type: "link",
+                        url: link,
+                        children: [{ type: "text", value: title }],
+                      },
+                      { type: "html", value: "<br/>" },
+                      //{
+                      //  type: "emphasis",
+                      //  children: [{ type: "text", value: contentSnippet }],
+                      //},
+                    ],
+                  },
+                ],
+              };
+            }
+          ),
+        },
+        end,
+      ];
+    });
+  };
+}
---
+++ profile/README.md
@@ -0,0 +1,34 @@
+<br />
+<h1>
+<p align="center">
+  <img src="/profile/images/extrapreneur-logo.png" alt="Logo" width="128" height="128">
+  <br>Extrapreneur - Think contribution
+</h1>
+  <p align="center">
+   We strive to impress our customers and create proud employees. Our team of specialists is here to support you along your journey to digitization.
+  </p>
+</p>
+
+## About us at Extrapreneur
+
+<!--about start-->
+
+Our name reflects our identity and what customers can expect from us. \<strong>Extrapreneur is a consultancy of IT professionals\</strong> on a journey to contribute to the development of both our customers and colleagues, and we show this through our drive to make things happen. We have embarked on a journey to impress customers and create proud employees.
+
+<!--about end-->
+
+## What is happening in Extrapreneur - Latest posts
+
+<!-- blog start -->
+
+* [How to apply my verification knowledge in Automotive industry?](https://www.extrapreneur.se/blog/how-to-apply-my-verification-knowledge-in-automotive-industry)<br/>
+
+* [Future of driving?](https://www.extrapreneur.se/blog/future-of-driving)<br/>
+
+* [Experience: Evolution of a CAB forum](https://www.extrapreneur.se/blog/experience-evolution-of-a-cab-forum)<br/>
+
+* [Hur kan du förbättra din kommunikationsförmåga?](https://www.extrapreneur.se/blog/hur-kan-du-frbttra-din-kommunikationsfrmga)<br/>
+
+* [Datastrategi del 2: Bästa BI-organisationen](https://www.extrapreneur.se/blog/datastrategi-del-2-bsta-bi-organisationen)<br/>
+
+<!-- blog end -->
