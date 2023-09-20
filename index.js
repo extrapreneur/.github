29c2606d5014ac6ff3ab43c7d1cf6adab6299275
psilore <psiloc@gmail.com>
2023-09-20 17:25:04 +0200

    update

--- index.js
+++ index.js
@@ -1,92 +1,47 @@
-//adding libraries
-
-//const puppeteer = require('puppeteer');
 import puppeteer from "puppeteer";
-
-//const RssParser = require("rss-parser");
 import Parser from "rss-parser";
-let parser = new Parser();
-
-//const vfile = require("to-vfile");
 import { toVFile, readSync } from "to-vfile";
-
-//const remark = require("remark");
 import { remark } from "remark";
-
-//const zone = require("mdast-zone");
 import { zone } from "mdast-zone";
-
-//const { promisify } = require("util");
 import { promisify } from "util";
-
-//const { writeFile } = require("fs");
 import { writeFile } from "fs";
-
-//const { join } = require("path");
 import { join } from "path";
-
 import path from "path";
 import { fileURLToPath } from "url";
-import { constants } from "buffer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const readmePath = join(__dirname, "profile/README.md");
 
-// Rip about us first paragraph and blog links
-const getAboutUsString = await puppeteer.launch({headless: 'new'}).then(async (browser) => {
-  const p = await browser.newPage();
-  
-
-  await p.setViewport({ width: 1000, height: 500 });
-  await p.goto("https://www.extrapreneur.se/en/about-us", { timeout: 0 });
-  const result = await p.evaluate(
+async function getAboutUsString() {
+  console.log("Starting getAboutUsString...");
+  const browser = await puppeteer.launch({ headless: "new" });
+  console.log("Browser launched...");
+  const page = await browser.newPage();
+  console.log("New page created...");
+  const navigationTimeout = 30000; // Increase the timeout to 30 seconds
+  await page.setDefaultNavigationTimeout(navigationTimeout); // Set the navigation timeout
+  await page.setViewport({ width: 1000, height: 500 });
+  console.log("Navigating to the page...");
+  await page.goto("https://www.extrapreneur.se/en/about-us");
+  console.log("Page navigated...");
+  const result = await page.evaluate(
     () =>
       document.querySelector(
         "#block-a69218aa3038e836d047 > div > div > p:nth-child(2)"
       ).innerHTML
   );
+  console.log("Content extracted...");
   await browser.close();
+  console.log("Browser closed...");
+  console.log("getAboutUsString completed.");
   return result;
-  // try {
-  //   const p = await browser.newPage();
-  //   await p.setViewport({ width: 1000, height: 500 });
-  //   await p.goto("https://www.extrapreneur.se/en/about-us");
+}
 
-  //   // const result = await p.evaluate(
-  //   //   () =>
-  //   //     document.querySelector("#block-a69218aa3038e836d047 > div > p").innerHTML
-  //   // );
+async function refreshAboutUsParagraph() {
+  const aboutUsString = await getAboutUsString();
+  const string = aboutUsString.split("\\").join("").trim();
 
-  //   const result = await p.evaluate(
-  //     () =>
-  //       document.querySelector(
-  //         "#block-a69218aa3038e836d047 > div > div > p:nth-child(2)"
-  //       ).innerHTML
-  //   );
-  //   /*   await p.goto("https://www.extrapreneur.se/blog");
-  //   const blogPostLinks = await p.evaluate(() =>
-  //     Array.from(document.getElementsByTagName("h1"), (e) => e.innerHTML)
-  //   ); */
-  //   return result;
-  // } catch (error) {
-  //   console.log('An error occurred while navigating to the page:', error);
-  // } finally {
-  //     await browser.close();
-  // }
-});
-// console.log(getAboutUsString);
-const file = await remark()
-  .use(refreshAboutUsParagraph)
-  .process(toVFile.readSync(readmePath));
-await promisify(writeFile)(readmePath, String(file));
-
-function refreshAboutUsParagraph() {
-  let string = getAboutUsString.split("\\").join("").trim();
-
-  // let str = theText.split("A").join("");
-  console.log(string)
   return (tree) => {
     zone(tree, "about", (start, nodes, end) => [
       start,
@@ -100,6 +55,45 @@ function refreshAboutUsParagraph() {
 }
 
 (async () => {
+  const file = await remark()
+    .use(refreshAboutUsParagraph)
+    .process(toVFile.readSync(readmePath));
+  await promisify(writeFile)(readmePath, String(file));
+})();
+
+async function refreshBlogPosts(feedItems) {
+  return () => (tree) => {
+    zone(tree, "blog", (start, nodes, end) => {
+      return [
+        start,
+        {
+          type: "list",
+          ordered: false,
+          children: feedItems.map(({ title, link }) => ({
+            type: "listItem",
+            children: [
+              {
+                type: "paragraph",
+                children: [
+                  {
+                    type: "link",
+                    url: link,
+                    children: [{ type: "text", value: title }],
+                  },
+                  { type: "html", value: "<br/>" },
+                ],
+              },
+            ],
+          })),
+        },
+        end,
+      ];
+    });
+  };
+}
+
+(async () => {
+  const parser = new Parser();
   const feed = await parser.parseURL(
     "https://www.extrapreneur.se/blog?format=rss"
   );
@@ -108,47 +102,3 @@ function refreshAboutUsParagraph() {
     .process(toVFile.readSync(readmePath));
   await promisify(writeFile)(readmePath, String(file));
 })();
-
-function refreshBlogPosts(feedItems) {
-
-
-  
-  //console.log(feedItems);
-  return () => (tree) => {
-    zone(tree, "blog", (start, nodes, end) => {
-      return [
-        start,
-        {
-          type: "list",
-          ordered: false,
-          children: feedItems.map(
-            ({ title, link, contentSnippet, pubDate }) => {
-              console.log(title,link)
-              return {
-                type: "listItem",
-                children: [
-                  {
-                    type: "paragraph",
-                    children: [
-                      {
-                        type: "link",
-                        url: link,
-                        children: [{ type: "text", value: title }],
-                      },
-                      { type: "html", value: "<br/>" },
-                      //{
-                      //  type: "emphasis",
-                      //  children: [{ type: "text", value: contentSnippet }],
-                      //},
-                    ],
-                  },
-                ],
-              };
-            }
-          ),
-        },
-        end,
-      ];
-    });
-  };
-}
