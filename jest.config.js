ef60c6c8bae3aaeca6fa93d73bbbf2dfdacab828
psilore <psiloc@gmail.com>
2025-07-08 17:18:50 +0200

    chore: update tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 export default {
-  testEnvironment: "jest-environment-node",
   testTimeout: 120000,
   testMatch: ["**/*.test.mjs"],
   testEnvironment: 'node',
--- tests/app.test.mjs
+++ tests/app.test.mjs
@@ -5,7 +5,6 @@ const indexUrl = "https://www.extrapreneur.se/en/home";
 const postsUrl = "https://www.extrapreneur.se/blog";
 
 describe("Scraping Tests", () => {
-  jest.setTimeout(120000); // <-- Move here
 
   let browser;
   let page;
